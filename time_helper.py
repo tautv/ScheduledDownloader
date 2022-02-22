4ab6944a8e4ee67c154e35684f987a5e49b48ca3
T <T.V@work>
2022-02-22 12:36:07 +0000

    Time Helper

--- time_helper.py
+++ time_helper.py
@@ -11,62 +11,20 @@ def GetTimestamp():
     second = time.strftime("%S")
     return "%s/%s/%s %s:%s:%s" % (year, month, day, hour, minute, second)
 
+def ShouldDownload(_last_download_time, _frequency):
+    now = datetime.datetime.strptime(GetTimestamp(), "%Y/%m/%d %H:%M:%S")
+    last = datetime.datetime.strptime(_last_download_time, "%Y/%m/%d %H:%M:%S")
+    diff = now-last
+    freq = datetime.datetime.strptime(_frequency, "%H:%M:%S")
+    freq_td = datetime.timedelta(hours=freq.hour, minutes=freq.minute, seconds=freq.second)
+    if(diff>freq_td):
+        return True
+    else:
+        return False
 
-
-strptime_string = "%Y/%m/%d %H:%M:%S"
-
-now = datetime.datetime.strptime(GetTimestamp(), "%Y/%m/%d %H:%M:%S")
-later = now + datetime.timedelta(hours=5)
-
-print(now)
-print(later)
-
-#https://www.w3schools.com/python/python_datetime.asp
-
-#
-#
-# print('------------')
-# today = datetime.datetime.now()
-# tomorrow = today + datetime.timedelta(days=1)
-# print('Today:    %s' % today)
-# print('Tomorrow: %s' % tomorrow)
-#
-# print('------------')
-# now = datetime.datetime.now()
-# hourFromNow = now + datetime.timedelta(hours=1)
-# print('Today:    %s' % now)
-# print('Tomorrow: %s' % hourFromNow)
-#
-# print('------------')
-# time = datetime.datetime.now()
-# year = time.strftime("%Y")
-# month = time.strftime("%m")
-# day = time.strftime("%d")
-# hour = time.strftime("%H")
-# minute = time.strftime("%M")
-# second = time.strftime("%S")
-#
-# print("Time Right now: %s" %time)
-# print("Year:\t%s" % year)
-# print("Month:\t%s" % month)
-# print("Day:\t%s" % day)
-# print("Hour:\t%s" % hour)
-# print("Minute:\t%s" % minute)
-# print("Second:\t%s" % second)
-#
-# print('------------')
-# custom = datetime.datetime(2020, 12, 31, 23, 59, 59) # making a custom date
-# print(custom)
-# custom2 = custom+datetime.timedelta(seconds=1) # custom date with 1 second added
-# print(custom2)
-# print('custom2-custom: %s' % (custom2-custom)) # time difference between custom2 and custom
-# print('custom>custom2: %s' % (custom>custom2)) # is custom greater than custom2
-# print('custom2>custom: %s' % (custom2>custom)) # is custom2 greater than custom
-# print('custom==custom2: %s' % (custom==custom2)) # is custom the same as custom2
-# print('custom2-custom == datetime.timedelta(seconds=1): %s' % (custom2-custom == datetime.timedelta(seconds=1))) # is the difference between custom and custom2 equal to 1 second
-#
-# print('------------')
-# '''
-#
-# '''
-#https://www.w3schools.com/python/python_datetime.asp
+def IsValidFrequency(_frequency):
+    try:
+        d = datetime.datetime.strptime(_frequency, "%H:%M:%S")
+        return True
+    except Exception as e:
+        return False
