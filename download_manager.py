fa805351cc31c53bd2fa18f18c7eaae4608693a4
GRaven <GRaven@gmx.us>
2022-04-20 00:01:43 +0100

    Fixed

--- download_manager.py
+++ download_manager.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
+import time
+import os
 from threading import Thread
 import urllib.request
 import ssl
 from urllib.parse import urlparse
-import os
 import event_manager
 from configs import GetValue
 # from configs import SetValue
@@ -13,15 +14,17 @@ from configs import GetValue
 class Downloader(Thread):
     def __init__(self, _id):
         super().__init__()
+        self.daemon = True  # kills the thread when application quits
         self._id = _id
         self.url = GetValue(self._id, 'url')
         self.event = event_manager.Event(str(_id))
         self.stopped = False
+        self._last_message_timer = time.time()
         # need this flag
         ssl._create_default_https_context = ssl._create_unverified_context
         # parse the URL; if it can't find the extension, try HTTP Head:
         _fname = os.path.basename(urlparse(self.url).path)
-        if '.' not in _fname:
+        if '.' not in _fname[-6:]:
             try:
                 req = urllib.request.Request(self.url, method='HEAD')
                 r = urllib.request.urlopen(req)
@@ -45,35 +48,31 @@ class Downloader(Thread):
         self.stopped = True
 
     def Handle_Progress(self, blocknum, blocksize, totalsize):
-        if not self.stopped:
-            if totalsize < 0:
+        if self.stopped:
+            raise
+        time_now = time.time()
+        if totalsize <= 0:
+            if time_now - self._last_message_timer > 0.1:
                 self.event.SendMessage((self._id, "NoTotalSize"))
-            read_data = blocknum * blocksize
-            if totalsize > 0:
-                self.download_percentage = read_data * 100 / totalsize
-                # slow down the return of the percentages:
-                if int(self.download_percentage) % 5 == 0:
-                    self.event.SendMessage(
-                        (self._id, self.download_percentage))
+        read_data = blocknum * blocksize
+        if totalsize > 0:
+            self.download_percentage = read_data * 100 / totalsize
+            if time_now-self._last_message_timer > 0.1:
+                self.event.SendMessage((self._id, self.download_percentage))
+                self._last_message_timer = time.time()
 
     def run(self):
         if not self.stopped:
             self.event.SendMessage((self._id, self.download_percentage))
-            if self.dest_path != '':
-                try:
-                    urllib.request.urlretrieve(
-                        self.url, self.dest_path, self.Handle_Progress)
-                except Exception as e:
-                    print(e)
-                    self.event.SendMessage((self._id, "Error"))
-                    self.StopThread()
-            else:
+            if self.dest_path == '':
+                raise Exception("Destination path is empty")
+            try:
+                urllib.request.urlretrieve(
+                    self.url, self.dest_path, self.Handle_Progress)
+                self.event.SendMessage((self._id, self.download_percentage))
+                self.event.SendMessage((self._id, "Finished"))
+            except Exception as e:
+                print(e)
                 self.event.SendMessage((self._id, "Error"))
-                raise Exception('Destination is empty!')
-            # end of main job
-            self.event.SendMessage((self._id, self.download_percentage))
-            self.event.SendMessage((self._id, "Finished"))
-            # could set it here, or through GUI:
-            # self.url = SetValue(self._id, 'last_download_time', time_helper.GetTimestamp())
         else:
             self.event.SendMessage((self._id, "Stopped"))
